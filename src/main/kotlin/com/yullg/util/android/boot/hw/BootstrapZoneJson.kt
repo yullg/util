1903f4b50711e23f06b7b352a0d613ba768c1bd1
yullg <yullg@email.com>
2023-01-05 14:46:53 +0800

    none

--- src/main/kotlin/com/yullg/util/android/boot/hw/BootstrapZoneJson.kt
+++ src/main/kotlin/com/yullg/util/android/boot/hw/BootstrapZoneJson.kt
@@ -12,35 +12,28 @@ fun main(args: Array<String>) {
     println("------- over ------")
 }
 
-private fun importFromExcel() = ExcelReader(File("E:\\世界时钟.xlsx")).use { reader ->
-    val jsonFile = File("D:\\Workspace\\huawo\\android\\hawofit\\module-service\\src\\main\\assets\\timezones.json")
-    val languageName = "fa"
-    val baseLanguageName = "en"
-    val sheetIndex = 0
-    val identifyColumnIndex = 0
-    val nameColumnIndex = 3
-    val firstRowNum = reader.getFirstRowNum(sheetIndex)
-    val lastRowNum = reader.getLastRowNum(sheetIndex)
-    val json = Gson().fromJson(jsonFile.readText(), JsonObject::class.java)
-    val jsonArray = JsonArray()
-    json.getAsJsonArray(baseLanguageName).forEach { baseJsonElement ->
-        val baseIdentify = baseJsonElement.asJsonObject.get("identify").asString
+private fun importFromExcel() =
+    ExcelReader(File("D:\\workspace\\huawo\\多语言翻译\\世界时钟（高棉语，希伯来语，波斯语）.xlsx")).use { reader ->
+        val jsonFile = File("D:\\workspace\\huawo\\多语言翻译\\timezones.json")
+        val languageName = "fa"
+        val sheetIndex = 0
+        val identifyColumnIndex = 0
+        val nameColumnIndex = 3
+        val firstRowNum = 1
+        val lastRowNum = 558
+        val json = Gson().fromJson(jsonFile.readText(), JsonObject::class.java)
+        val jsonArray = JsonArray()
         for (rowNum in firstRowNum..lastRowNum) {
-            val identify = reader.readString(sheetIndex, rowNum, identifyColumnIndex)
-            if (identify != null && baseIdentify == identify) {
-                val name = reader.readString(sheetIndex, rowNum, nameColumnIndex)!!
-                jsonArray.add(JsonObject().apply {
-                    addProperty("identify", identify)
-                    addProperty("name", name)
-                })
-                break
-            }
+            val identify = reader.readString(sheetIndex, rowNum, identifyColumnIndex)!!
+            val name = reader.readString(sheetIndex, rowNum, nameColumnIndex)!!
+            jsonArray.add(JsonObject().apply {
+                addProperty("identify", identify)
+                addProperty("name", name)
+            })
         }
+        json.add(languageName, jsonArray)
+        val jsonText = GsonBuilder().apply {
+            setPrettyPrinting()
+        }.create().toJson(json)
+        jsonFile.writeText(jsonText)
     }
-    if (jsonArray.size() != json.getAsJsonArray(baseLanguageName).size()) throw IllegalArgumentException()
-    json.add(languageName, jsonArray)
-    val jsonText = GsonBuilder().apply {
-        setPrettyPrinting()
-    }.create().toJson(json)
-    jsonFile.writeText(jsonText)
-}
